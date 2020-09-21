ee09c40d43f44609b7ccc6a6d936feb2f9f3e91f
Anna Cook <acook.adc@gmail.com>
2020-09-21 17:01:00 -0400

    end end day 9/21

--- Buoy.R
+++ Buoy.R
@@ -78,35 +78,3 @@ abline(yhat, col="red")
 
 ##in April and August 2018, there was no recorded data due to lack of funding.
 
-
-
-
-#MR2 <- transform(MR, YYYY=as.numeric(YYYY), ATMP= as.numeric(ATMP))
-
-##MR2 <- filter(MR, DD==11, hh==12, ATMP<500)
-
-##MR3 <- select(MR2, YYYY, MM, ATMP)
-
-##MR4 <- transform(MR3, YYYY= as.numeric(YYYY), MM= as.numeric(MM), ATMP= as.numeric(ATMP))
-
-#MR5 <- unite(MR4, DATE, YYYY, MM, sep = "-")
-##MR5 <- mutate(MR4, DATE = 12 * (YYYY-2000) + MM)
-
-##ggplot(data=MR5, aes(DATE, ATMP)) + geom_point() + geom_line()
-
-##fit <- stan_glm(ATMP ~ DATE, data = MR5, refresh=0)
-##fit
-
-##MR7 <- filter(MR, MM==11, DD==11, hh==12, ATMP<500)
-##MR8 <- transform(MR7, YYYY= as.numeric(YYYY), ATMP= as.numeric(ATMP))
-
-##ggplot(data=MR8, aes(YYYY,ATMP)) + geom_point()
-##fit2 <- stan_glm(ATMP ~ YYYY, data=MR8, refresh=0)
-##fit2
-
-##group_by() and summarize() mean of each group together in dplyr package
-
-#Good idea!
-
-
-##file2 <- select(file, YYYY, MM, DD, hh, ATMP)
